dd4598324adc1d48af5850e9c395bdc7fb8b88d0
Eduard <eddirworkmail@gmail.com>
2021-01-26 12:28:42 +0400

    Запрет индексации сайта

--- HostPanel/urls.py
+++ HostPanel/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 from django.conf.urls import include
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('', include('panel.urls')),
     path('admin/', admin.site.urls),
+    path(
+        "robots.txt",
+        TemplateView.as_view(template_name="robots.txt", content_type="text/plain"),
+    ),
 ]
