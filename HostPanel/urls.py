90f97203d2eeb00a72bbb458db738cdd3ca5a3ac
Eduard <mail@rostkov.pro>
2021-04-08 18:56:30 +0400

    Favicon

--- HostPanel/urls.py
+++ HostPanel/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import include
 from django.contrib import admin
+from django.contrib.staticfiles.storage import staticfiles_storage
 from django.urls import path
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, RedirectView
 
 urlpatterns = [
     path('', include('panel.urls')),
@@ -24,5 +25,9 @@ urlpatterns = [
     path(
         "robots.txt",
         TemplateView.as_view(template_name="robots.txt", content_type="text/plain"),
+    ),
+    path(
+        "favicon.ico",
+        RedirectView.as_view(url=staticfiles_storage.url("favicon.ico")),
     ),
 ]
