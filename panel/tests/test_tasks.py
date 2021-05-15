db59afd523632991dc19deaa37a7bcc5522a2521
Eduard <mail@rostkov.pro>
2021-05-15 23:13:47 +0400

    Close #2

--- panel/tests/test_tasks.py
+++ panel/tests/test_tasks.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 
-from panel.models import MPackage, Dedic, Server, Status
+from panel.models import MPackage, Dedic, Server, Status, SRPackage
 from panel.tasks.DedicUnit import DedicUnit
 from panel.tasks.ServerUnit import ServerUnit
 
@@ -8,17 +8,24 @@ from panel.tasks.ServerUnit import ServerUnit
 class TasksTestCase(TestCase):
 
     def test_servers_create(self):
-        mpackage = MPackage.objects.create(
+        m_package = MPackage.objects.create(
             name="MPackage test",
             master="packages/Master.zip"
         )
-        mpackage.save()
+        m_package.save()
 
+        sr_package = SRPackage.objects.create(
+            name="SRPackage test",
+            spawner="packages/Spawner.zip",
+            room="packages/Room.zip"
+        )
+
+        # Создание дедика
         dedic = Dedic.objects.create(
             name="Dedic Master test",
             ip="5.180.138.187",
             user_root="root",
-            user_single="unittest1",
+            user_single="unittest",
             password_root="CHBE644Q7x82",
             ssh_key=False
         )
@@ -38,29 +45,46 @@ class TasksTestCase(TestCase):
         dedic.refresh_from_db()
         self.assertNotEqual(last_listen, dedic.last_listen)
 
+        # Создание мастера
         master = Server.objects.create(
             dedic=dedic,
             name="Master test",
-            package=mpackage
+            package=m_package
         )
         master.save()
 
-        s = ServerUnit(master)
-        s.init()
-
-        self.assertEqual(master.get_last_status(), None)
-
-        s.stop()
-        master.refresh_from_db()
-        self.assertEqual(master.get_last_status().condition, Status.Condition.STOPPED)
+        s = self._test_server(master)
 
         s.update_caretaker()
         master.refresh_from_db()
         self.assertTrue("Обновление Caretaker завершено" in master.log)
         self.assertEqual(master.get_last_status().condition, Status.Condition.STOPPED)
 
+        # Создание спавнера
+        spawner = Server.objects.create(
+            parent=master,
+            dedic=dedic,
+            name="Spawner test",
+            package=sr_package
+        )
+        spawner.save()
+
+        self._test_server(spawner)
+
+        # Сборка мусора
         s.delete()
         d.delete()
 
         print("[*] Тест фоновых задач пройден!")
 
+    def _test_server(self, server):
+        s = ServerUnit(server)
+        s.init()
+
+        self.assertEqual(server.get_last_status(), None)
+
+        s.stop()
+        server.refresh_from_db()
+        self.assertEqual(server.get_last_status().condition, Status.Condition.STOPPED)
+
+        return s
