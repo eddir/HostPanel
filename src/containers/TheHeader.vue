106218596fcef72afaef69cb278c3b8e07430c00
Eduard <eddirworkmail@gmail.com>
2021-07-01 11:57:26 +0400

    Убраны неиспользуемые разделы и тесты.

--- src/containers/TheHeader.vue
+++ src/containers/TheHeader.vue
@@ -19,16 +19,6 @@
           Dashboard
         </CHeaderNavLink>
       </CHeaderNavItem>
-      <CHeaderNavItem class="px-3">
-        <CHeaderNavLink to="/users" exact>
-          Users
-        </CHeaderNavLink>
-      </CHeaderNavItem>
-      <CHeaderNavItem class="px-3">
-        <CHeaderNavLink>
-          Settings
-        </CHeaderNavLink>
-      </CHeaderNavItem>
     </CHeaderNav>
     <CHeaderNav class="mr-4">
       <CHeaderNavItem class="d-md-down-none mx-2">
